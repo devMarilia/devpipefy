04722aa2a4313da255db25c7234997002f3505e7
devMarilia <marilia.lemos753@gmail.com>
2020-05-23 15:17:32 -0300

    Foi reaclizado a criaçao do component Header e realizado o impor no arquivo pricipal do projeto.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Header from './components/Header'
 
 
 function App() {
   return (
+    <>
+    <Header/>
+
+    
     <GlobalStyle/>
+    </>
   );
 }
 
