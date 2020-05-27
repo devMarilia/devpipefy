3eeee71fc40bb89459333aacd094f38c1839f367
devMarilia <marilia.lemos753@gmail.com>
2020-05-27 15:50:46 -0300

    Implementado a logica e a iimplementaçao do useDrop

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import produce from 'immer';
+import BoardContex from './context';
 import List from '../List';
 import { loadLists } from '../../services/api';
 import { Container } from './styles';
 
-const lists = loadLists();
+const data = loadLists();
 
 function Board() {
+  const [lists, setList] = useState(data);
+
+  function move(fromList, toList, from, to) {
+    console.log(fromList);
+    setList(produce(lists, draft => {
+      const dragget = draft[fromList].cards[from];
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragget);
+    }));
+  }
   return (
-    <Container >
-      {lists.map(list => <List  key={list.title} data={list} />)}
-      
-    </Container >
+    <BoardContex.Provider value={{ lists, move }}>
+      <Container >
+        {lists.map((list, index)=> <List  key={list.title} index={index} data={list} />)}
+      </Container >
+    </BoardContex.Provider>
   );
 }
 
